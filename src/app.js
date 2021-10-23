3699069228fc47240cbc2c5e66f809c62f274227
Mert Ercan <mert.ercan@istegelsin.com>
2021-10-23 15:57:21 +0300

    fix for delay between requests

--- src/app.js
+++ src/app.js
@@ -2,10 +2,22 @@
 const { parseProductUrls } = require('./components/product/productUrlHelper');
 const productParser = require('./components/product/productParser');
 
-const textWithProductUrl = 'Ben bir sürü ürün içeriyorum! https://www.amazon.com.tr/gp/product/6052998385/ref=ox_sc_saved_image_4?smid=A1UNQM1SR2CHM&psc=1 https://www.amazon.com.tr/gp/product/B091Q7CG4N/ref=ox_sc_saved_image_3?smid=&psc=1 https://www.amazon.com.tr/gp/product/B08MVBTSGL/ref=ox_sc_saved_image_7?smid=A1UNQM1SR2CHM&psc=1 https://www.amazon.com.tr/gp/product/B08XZR8PD5/ref=ox_sc_act_image_11?smid=A1UNQM1SR2CHM&psc=1';
-const parsedProductUrls = parseProductUrls(textWithProductUrl);
+const main = async () => {
+  const testProducts = [
+    'https://www.amazon.com.tr/gp/product/B091Q7CG4N/ref=ox_sc_saved_image_3?smid=&psc=1', // stok dışı
+    'https://www.amazon.com.tr/gp/product/B08XZR8PD5/ref=ox_sc_act_image_10?smid=A1UNQM1SR2CHM&psc=1', // çay
+    'https://www.amazon.com.tr/gp/product/6052998385/ref=ox_sc_saved_image_4?smid=A1UNQM1SR2CHM&psc=1', // kitap
+    'https://www.amazon.com.tr/gp/product/B08BHTY15P/ref=ox_sc_saved_image_6?smid=A1UNQM1SR2CHM&psc=1', // ayakkabı
+    'https://www.amazon.com.tr/gp/product/B08J45VJ8Y/ref=ox_sc_saved_image_6?smid=A1UNQM1SR2CHM&psc=1', // mutfak
+    'https://www.amazon.com.tr/gp/product/B08P23Y9YR/ref=ox_sc_saved_image_8?smid=A1HN1W25VYST46&psc=1', // farklı satıcı ama prime
+    'https://www.amazon.com.tr/gp/product/B07CVBSCCH/ref=ox_sc_saved_image_7?smid=A3O5TP4R0OZYXZ&psc=1' // yurtdışı ürün
+  ].join(' ');
+  const parsedProductUrls = parseProductUrls(testProducts);
 
-parsedProductUrls.forEach(async productUrl => {
-  await productParser(productUrl);
-  await new Promise(r => setTimeout(r, 1000));
-});
+  for (const productUrl of parsedProductUrls) {
+    await productParser(productUrl);
+    await new Promise(r => setTimeout(r, 1400));
+  }
+};
+
+main();
