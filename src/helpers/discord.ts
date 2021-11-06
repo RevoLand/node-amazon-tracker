eb5006e01fb28d9fec8c0bd893f3fd480c297772
Mert Ercan <mert.ercan@istegelsin.com>
2021-11-06 14:38:26 +0300

    Discord Improvements 2.0 #8

--- src/helpers/discord.ts
+++ src/helpers/discord.ts
@@ -1,10 +1,17 @@
 
 import { Client, Intents } from 'discord.js';
 import { exit } from 'process';
+import { productTrackers } from '../app';
 import { readDiscordCommands, registerDiscordCommands } from '../components/discord/discordCommands';
 import discordReadyEvent from '../components/discord/events/discordReadyEvent';
+import { ProductTracker } from '../components/product/producttracker';
 import discordConfig from '../config/discord';
+import { ProductController } from '../controller/ProductController';
+import { SettingController } from '../controller/SettingController';
+import productCreatedEmbed from './embeds/productCreatedEmbed';
+import productUpdated from './embeds/productUpdatedEmbed';
 import { ExitCodesEnum } from './enums/ExitCodesEnum';
+import { parseProductUrls } from './productUrlHelper';
 
 export const connectToDiscord = async (): Promise<Client> => {
   if (!discordConfig.botToken) {
@@ -40,6 +47,53 @@ export const connectToDiscord = async (): Promise<Client> => {
       } catch (error) {
         console.error(error);
         return interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+      }
+    });
+
+    // TODO:
+    // Refactor with commands/track.ts
+    client.on('messageCreate', async message => {
+      if (message.content.startsWith('!track ') || message.content.startsWith('!takip ')) {
+        const productUrls = parseProductUrls(message.content);
+
+        if (productUrls.length === 0) {
+          await message.reply({
+            content: 'Girilen mesaj takibe uygun ürün içermiyor 😢'
+          });
+
+          return;
+        }
+
+        await message.reply({
+          content: `Aşağıdaki ürün(ler) takibe alınacak:\`\`\`${productUrls.join('\n')}\`\`\``
+        });
+
+        try {
+          const productTracker = productTrackers.find(_productTracker => _productTracker.country === 'discord') ?? new ProductTracker('discord', await SettingController.getAll(), client);
+
+          if (!productTracker.browser) {
+            await productTracker.setBrowser();
+          }
+
+          for (const productUrl of productUrls) {
+            const createProductResult = await ProductController.createProductFromUrl(productUrl, productTracker);
+            if (!createProductResult) {
+              console.error('No product detail returned for product url, skipping.', productUrl);
+              continue;
+            }
+
+            const productEmbed = createProductResult.existing_product_detail ?
+              productUpdated(createProductResult.productDetail) :
+              productCreatedEmbed(createProductResult.productDetail);
+            await message.channel.send({
+              embeds: [productEmbed]
+            })
+          }
+        } catch (error) {
+          console.error('An error happened while executing the !track command function.', error);
+
+          exit(ExitCodesEnum.TrackCommandFailed);
+        }
       }
     });
 
